980f69771b7781907086ca66028c2637b48f1b9c
guasam <guasamos@outlook.com>
2025-05-31 16:14:43 +0530

    Move welcome kit to components dir

--- app/app.tsx
+++ app/app.tsx
@@ -1,9 +1,5 @@
-import { Button } from '@/app/components/ui/button'
+import WelcomeKit from '@/app/components/welcome/WelcomeKit'
 
 export default function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click me</Button>
-    </div>
-  )
+  return <WelcomeKit />
 }
